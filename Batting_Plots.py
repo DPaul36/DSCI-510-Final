9562bdbe1e45d0dcc8e8ba99ea11e2b3af8c198e
DPaul36 <dppaul@usc.edu>
2022-12-12 22:36:50 -0800

    Report

--- Batting_Plots.py
+++ Batting_Plots.py
@@ -12,22 +12,22 @@ import matplotlib.pyplot as plt
 
 teams_list = ['HOU','SEA','ATL','NYY','NYM','LAD','SD','PHI','TB','STL','TOR','CLE']
 Months = ["Apr",'May','June','July','Aug','Sep','Oct']
-#month_team_data = cf.month_data(batting_stats_range)
+month_team_data = cf.month_data(batting_stats_range)
 
 
 
-#houston_ops = cf.month_ops('Houston',month_team_data)
-#yankees_ops = cf.month_ops('New York Yankees',month_team_data)
-#dodgers_ops = cf.month_ops('Los Angeles Dodgers',month_team_data)
-#mets_ops = cf.month_ops('New York Mets',month_team_data)
-#philly_ops = cf.month_ops('Philadelphia',month_team_data)
-#braves_ops = cf.month_ops('Atlanta',month_team_data)
-#padres_ops = cf.month_ops('San Diego',month_team_data)
-#guard_ops = cf.month_ops('Cleveland',month_team_data)
-#seattle_ops = cf.month_ops('Seattle',month_team_data)
-#tampa_ops = cf.month_ops('Tampa Bay',month_team_data)
-#cards_ops = cf.month_ops('St. Louis',month_team_data)
-#toronto_ops = cf.month_ops('Toronto',month_team_data)
+houston_ops = cf.month_ops('Houston',month_team_data)
+yankees_ops = cf.month_ops('New York Yankees',month_team_data)
+dodgers_ops = cf.month_ops('Los Angeles Dodgers',month_team_data)
+mets_ops = cf.month_ops('New York Mets',month_team_data)
+philly_ops = cf.month_ops('Philadelphia',month_team_data)
+braves_ops = cf.month_ops('Atlanta',month_team_data)
+padres_ops = cf.month_ops('San Diego',month_team_data)
+guard_ops = cf.month_ops('Cleveland',month_team_data)
+seattle_ops = cf.month_ops('Seattle',month_team_data)
+tampa_ops = cf.month_ops('Tampa Bay',month_team_data)
+cards_ops = cf.month_ops('St. Louis',month_team_data)
+toronto_ops = cf.month_ops('Toronto',month_team_data)
 
 
 team_statcast = cf.teams_statcast(teams_list)
@@ -152,22 +152,22 @@ def batting_plots():
     ax3.scatter(months,CLE[14:21],s=C,label = 'CLE')
     
     
-    ax4.scatter(months,ATL[21:28],s=A,label = 'ATL')
-    ax4.scatter(months,HOU[21:28],s=H,label = 'HOU')
-    ax4.scatter(months,NYY[21:28],s=N,label = 'NYY')
-    ax4.scatter(months,LAD[21:28],s=L,label = 'LAD')
-    ax4.scatter(months,SD[21:28],s=P,label = 'SD')
-    ax4.scatter(months,SEA[21:28],s=S,label = 'SEA')
-    ax4.scatter(months,PHI[21:28],s=PH,label = 'PHI')
-    ax4.scatter(months,CLE[21:28],s=C,label = 'CLE')
-    #ax4.scatter(months,houston_ops,s=H,label='HOU')
-    #ax4.scatter(months,braves_ops,s=A,label='ATL')
-    #ax4.scatter(months,philly_ops,s=PH,label='PHI')
-    #ax4.scatter(months,yankees_ops,s=N,label='NYY')
-    #ax4.scatter(months,guard_ops,s=C,label='CLE')
-    #ax4.scatter(months,dodgers_ops,s=L,label='LAD')
-    #ax4.scatter(months,padres_ops,s=P,label='SD')
-    #ax4.scatter(months,seattle_ops,s=S,label='SEA')
+    #ax4.scatter(months,ATL[21:28],s=A,label = 'ATL')
+    #ax4.scatter(months,HOU[21:28],s=H,label = 'HOU')
+    #ax4.scatter(months,NYY[21:28],s=N,label = 'NYY')
+    #ax4.scatter(months,LAD[21:28],s=L,label = 'LAD')
+    #ax4.scatter(months,SD[21:28],s=P,label = 'SD')
+    #ax4.scatter(months,SEA[21:28],s=S,label = 'SEA')
+    #ax4.scatter(months,PHI[21:28],s=PH,label = 'PHI')
+    #ax4.scatter(months,CLE[21:28],s=C,label = 'CLE')
+    ax4.scatter(months,houston_ops,s=H,label='HOU')
+    ax4.scatter(months,braves_ops,s=A,label='ATL')
+    ax4.scatter(months,philly_ops,s=PH,label='PHI')
+    ax4.scatter(months,yankees_ops,s=N,label='NYY')
+    ax4.scatter(months,guard_ops,s=C,label='CLE')
+    ax4.scatter(months,dodgers_ops,s=L,label='LAD')
+    ax4.scatter(months,padres_ops,s=P,label='SD')
+    ax4.scatter(months,seattle_ops,s=S,label='SEA')
 
     
     
@@ -188,12 +188,12 @@ def batting_plots():
     ax1.set_ylabel('Launch Velo x Angle')
     ax2.set_ylabel('Exit Velocity')
     ax3.set_ylabel('Launch Angle')
-    ax4.set_ylabel('Launch Velo x Angle')
-
-    ax1.set_title('Launch Angle x Velocity Runners on Base') 
-    ax2.set_title('Launch Angle')
-    ax3.set_title('Exit Velocity')
-    ax4.set_title('Launch Angle x Velocity')
+    ax4.set_ylabel('OPS')
+    ax4.set_ylim([.4,1.1])
+    ax1.set_title('Launch Angle x Velocity') 
+    ax3.set_title('Launch Angle')
+    ax2.set_title('Exit Velocity')
+    ax4.set_title('OPS')
 
     return ax1,ax2,ax3,ax4
 
@@ -228,14 +228,14 @@ def final_4():
     
     
     
-    ax4.scatter(months,HOU[21:28],s=H,label = 'HOU')
-    ax4.scatter(months,NYY[21:28],s=N,label = 'NYY')
-    ax4.scatter(months,SD[21:28],s=P,label = 'SD')
-    ax4.scatter(months,PHI[21:28],s=PH,label = 'PHI')
-    #ax4.scatter(months,houston_ops,s=H,label='HOU')
-    #ax4.scatter(months,philly_ops,s=PH,label='PHI')
-    #ax4.scatter(months,yankees_ops,s=N,label='NYY')
-    #ax4.scatter(months,padres_ops,s=P,label='SD')
+    #ax4.scatter(months,HOU[21:28],s=H,label = 'HOU')
+    #ax4.scatter(months,NYY[21:28],s=N,label = 'NYY')
+    #ax4.scatter(months,SD[21:28],s=P,label = 'SD')
+    #ax4.scatter(months,PHI[21:28],s=PH,label = 'PHI')
+    ax4.scatter(months,houston_ops,s=H,label='HOU')
+    ax4.scatter(months,philly_ops,s=PH,label='PHI')
+    ax4.scatter(months,yankees_ops,s=N,label='NYY')
+    ax4.scatter(months,padres_ops,s=P,label='SD')
 
     
     
@@ -256,11 +256,12 @@ def final_4():
     ax1.set_ylabel('Launch Velo x Angle')
     ax2.set_ylabel('Exit Velocity')
     ax3.set_ylabel('Launch Angle')
-    ax4.set_ylabel('Launch Velo x Angle')
+    ax4.set_ylabel('OPS')
+    ax4.set_ylim([.4,1.1])
 
-    ax1.set_title('Launch Angle x Velocity Runners on Base') 
-    ax2.set_title('Launch Angle')
-    ax3.set_title('Exit Velocity')
-    ax4.set_title('Launch Velo x Angle')
+    ax1.set_title('Launch Angle x Velocity') 
+    ax3.set_title('Launch Angle')
+    ax2.set_title('Exit Velocity')
+    ax4.set_title('OPS')
 
     return ax1,ax2,ax3,ax4
